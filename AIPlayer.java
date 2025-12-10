d767a1a015ed941f00d3a276748f88d99fd24717
HoseCode <hosecoin@outlook.com>
2025-12-10 22:39:54 +0800

    Update AIPlayer.java

--- AIPlayer.java
+++ AIPlayer.java
@@ -1,3 +1,6 @@
+
+import java.util.PriorityQueue;
+
 public class AIPlayer extends Player{
     // ============================================================
     // TODO: Implement chooseMove()
@@ -16,4 +19,48 @@ public class AIPlayer extends Player{
 
     // You may also add any other helper functions, variables,
     // and constructors needed for your implementation.
+
+    class Node {
+        int round;
+        int[] piecePositions;
+        int distance;
+
+        public Node(int round, int[] piecePositions, int distance) {
+            this.round = round;
+            this.piecePositions = piecePositions;
+            this.distance = distance;
+        }
+    }
+
+    @Override
+    public int chooseMove(GameState state) {
+        // TODO: This needs to be updated.
+        int round = state.getRound();
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {
+            return Integer.compare(a.distance, b.distance);
+        });
+
+        int distance = calculateDistance(round, state.piecePositions, state.diceSequence);
+        pq.add(new Node(round, state.piecePositions, distance));
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            state.setPiecePositions(node.piecePositions);
+            int[][] possibleMoves = state.generatePossibleMoves();
+            for (int[] possibleMove : possibleMoves) {
+                distance = calculateDistance(node.round, possibleMove, state.diceSequence);
+                pq.add(new Node(node.round, possibleMove, distance));
+            }
+        }
+        this.printMove(state);
+        return 0;
+    }
+
+    private int calculateDistance(int round, int[] piecePositions, int[] diceSequence) {
+        // TODO: This needs to be updated.
+        int[] diceCount = new int[6];
+        for (int i = round - 1; i < diceSequence.length; i++) {
+            diceCount[diceSequence[i]]++;
+        }
+        return 1;
+    }
 }
