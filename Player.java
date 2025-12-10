8337ad49a299a1813b37d26b8d32f4dfd8ccbd5d
HoseCode <hosecoin@outlook.com>
2025-12-10 22:39:42 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,12 +1,21 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public abstract class Player {
-    // ============================================================
-    // TODO: Implement printMove()
-    // ------------------------------------------------------------
-    // This method print the chosen move of the player into the
-    // "moves.txt" file
-    //
-    // You may decide on the return type, parameters, and internal logic.
-    // ============================================================
 
     public abstract int chooseMove(GameState state);
+    
+    public void printMove(GameState state) {
+        String filePath = "moves.txt";
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (int pos : state.getPiecePositions()) {
+                writer.write(pos + " ");
+            }
+            writer.write("\n");
+            
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
