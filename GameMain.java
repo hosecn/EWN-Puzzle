e0b9a49c61b641d68c427d63a3684062a229558b
HoseCode <hosecoin@outlook.com>
2025-12-10 21:51:18 +0800

    Update GameMain.java

--- GameMain.java
+++ GameMain.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GameMain {
 
     public static void main(String[] args) {
@@ -8,27 +11,51 @@ public class GameMain {
         //
         // The function should perform the following tasks:
         //
-        // 1. Prompt the user to choose a game mode:
-        //      - Human Player
-        //      - Random Player
-        //      - AI Player
-        //
-        // 2. Create a player object based on the selected mode:
-        //      - For Human Player => prompt for player name.
-        //      - For Random Player => use default name "Random Player".
-        //      - For AI Player => use default name "AI Player".
-        //
-        // 3. Prompt the user to select a level.
-        //
+        
+        System.out.println("Please choose a game mode.");
+        System.out.println("Human Player : Press 1");
+        System.out.println("Random Player : Press 2");
+        System.out.println("AI Player : Press 3");
+        Player player = null;
+
+        int playerNumber;
+        try (Scanner sc = new Scanner(System.in)) {
+            playerNumber = sc.nextInt();
+        }
+
+        switch (playerNumber) {
+            case 1 -> player = new HumanPlayer();
+            case 2 -> player = new RandomPlayer();
+            case 3 -> player = new AIPlayer();
+            default -> throw new AssertionError();
+        }
+
+        System.out.println("Please select a level.");
+        System.out.println("Press 1 / 2 / 3 / 4.");
+
+        int levelNumber;
+        try (Scanner sc = new Scanner(System.in)) {
+            levelNumber = sc.nextInt();
+        }
+        String filePath = "./TestCases/level" + (levelNumber - 1) + ".txt";
+        GameLoader loader = new GameLoader(filePath);
+        int targetPiece = loader.getTargetPiece();
+        int[] piecePositions = loader.getPiecePositions();
+        int[] diceSequence = loader.getDiceSequence();
+
+        loader.printGameDetails();
+
+        GameState state = new GameState(targetPiece, piecePositions, diceSequence, 1);
+
+        int result = player.chooseMove(state);
         // 4. Call the chooseMove() function of the player
         //    to perform their move based on the current game state.
         //
         // 5. Display the result of the game
-        //
-        // ============================================================
 
         // You may also add any other helper functions, variables,
         // and constructors needed for your implementation.
+        
     }
 }
 
