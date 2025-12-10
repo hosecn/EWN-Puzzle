b379c962ae185e9078bbdf646d0abff551acfd3f
HoseCode <hosecoin@outlook.com>
2025-12-10 22:06:18 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -8,15 +8,5 @@ public abstract class Player {
     // You may decide on the return type, parameters, and internal logic.
     // ============================================================
 
-    // ============================================================
-    // TODO: Implement abstract function - chooseMove()
-    // ------------------------------------------------------------
-    // This is an abstract method that defines how the player selects
-    // a move. You should implement the logic in the subclasses.
-    //
-    // You may decide on the return type, and parameters.
-    // ============================================================
-
-    // You may also add any other helper functions, variables,
-    // and constructors needed for your implementation.
+    public abstract int chooseMove(GameState state);
 }
