6454427a3739f30f20d6834e3bf69129d989bb0e
HoseCode <hosecoin@outlook.com>
2025-12-12 16:24:18 +0800

    Implement HumanPlayer.chooseMove()

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,16 +1,42 @@
-public class HumanPlayer extends Player{
-    // ============================================================
-    // TODO: Implement chooseMove()
-    // ------------------------------------------------------------
-    // This method prompts the human player to choose the next move
-    //
-    // You may decide on the return type, parameters, and logic.
-    // ============================================================
+import java.util.Scanner;
 
-    // You may also add any other helper functions, variables,
-    // and constructors needed for your implementation.
+public class HumanPlayer extends Player{
+
     @Override
     public int chooseMove(GameState state) {
-        return -1;
+        try (Scanner sc = new Scanner(System.in)){
+            for (int round = 1; round <= state.maxRound; round++) {
+                System.out.print("Current piece positions: ");
+                for (int i = 0; i < 6; i++) {
+                    System.out.print(state.piecePositions[i] + " ");
+                }
+                System.out.println();
+                System.out.println("Please choose your move: ");
+
+                int[][] possibleMoves = state.generatePossibleMoves(round);
+                
+                for (int i = 0; i < possibleMoves.length; i++) {
+                    System.out.print("Press " + (i + 1) + ": ");
+                    for (int j = 0; j < 6; j++) {
+                        System.out.print(possibleMoves[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+
+                try {
+                    int index = sc.nextInt();
+                    state.setPiecePositions(possibleMoves[index - 1]);                    
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+                if (state.isWinning()) {
+                    return 1;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
     }
 }
